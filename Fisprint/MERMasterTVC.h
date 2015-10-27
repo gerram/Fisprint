9e68d5a60acd384e6544782aa34757d412199e4a
George Malushkin <gmalyush@Georges-Mac-mini.local>
2015-10-27 18:23:32 +0300

    End of day

---
+++ Fisprint/MERMasterTVC.h
@@ -0,0 +1,13 @@
+//
+//  MERMasterTVC.h
+//  Fisprint
+//
+//  Created by George Malushkin on 27/10/15.
+//  Copyright © 2015 George Malushkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MERMasterTVC : UITableViewController
+
+@end
